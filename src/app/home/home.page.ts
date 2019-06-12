6ab98254869fc1ca04d85c617ece633b93cfb551
Silvio Cesar Marqui Limeira <silvio.limeira@mstech.com.br>
2019-06-12 08:04:09 -0300

    configure page for display the list of the bulletin board

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,72 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+
+import { AlertController, LoadingController } from "@ionic/angular";
+import { Router, ActivatedRoute } from "@angular/router";
+import * as firebase from "Firebase";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
 })
 export class HomePage {
+  infos = [];
+  ref = firebase.database().ref("infos/");
+  alertController: any;
 
-  constructor() {}
+  constructor(
+    public router: Router,
+    public loadingController: LoadingController
+  ) {
+    this.ref.on("value", resp => {
+      this.infos = [];
+      this.infos = snapshotToArray(resp);
+    });
+  }
 
+  addInfo() {
+    this.router.navigate(["/add-info"]);
+  }
+
+  edit(key) {
+    this.router.navigate(["/edit/" + key]);
+  }
+
+  async delete(key) {
+    const alert = await this.alertController.create({
+      header: "Confirm",
+      message: "Are you sure want to delete this info?",
+      buttons: [
+        {
+          text: "Cancel",
+          role: "cancel",
+          cssClass: "secondary",
+          handler: blah => {
+            console.log("cancel");
+          }
+        },
+        {
+          text: "Okay",
+          handler: () => {
+            firebase
+              .database()
+              .ref("infos/" + key)
+              .remove();
+          }
+        }
+      ]
+    });
+  }
 }
+
+export const snapshotToArray = snapshot => {
+  let returnArr = [];
+
+  snapshot.forEach(childSnapshot => {
+    let item = childSnapshot.val();
+    item.key = childSnapshot.key;
+    returnArr.push(item);
+  });
+
+  return returnArr;
+};
